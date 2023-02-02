4d6add254a93d90cd58bced173347f7c3cf5c429
unknown <Alan M. Souza>
2023-02-02 15:48:48 -0300

    testando git restore

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 Para rodar esse projeto na sua máquina, digite:
 
 ```
-node app.js
+npm install react
 ```
--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-console.log("Rodando o sistema de gerenciamento de jogoss")
+console .log("Rodando o sistema de gerenciamento de jogos")
